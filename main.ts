4fb52238befb7bcb13032baf4f4c37874781da37
zoe <zoster@users.noreply.github.com>
2020-12-11 15:00:45 -0500

    from the instructions

--- main.ts
+++ main.ts
@@ -1,39 +1,8 @@
-basic.showIcon(IconNames.Happy)
-TPBot.headlightColor(0xff9da5)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    music.playMelody("C5 A B G A F G E ", 480)
-    TPBot.setWheels(100, 0)
-    basic.pause(5000)
-    music.stopAllSounds()
-    TPBot.stopCar()
-    for (let index = 0; index <= 6; index++) {
-        basic.showLeds(`
-            . # . # .
-            . . . . .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
+    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 15)) {
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 1)
+        TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 0.5)
     }
 })
