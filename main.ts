542070a845ae6fe6d6b0a94e666c6d160f3be0f6
zoe <zoster@users.noreply.github.com>
2020-12-11 14:54:52 -0500

    Switching to two control block

--- main.ts
+++ main.ts
@@ -1,37 +1,39 @@
 basic.showIcon(IconNames.Happy)
-TPBot.headlightColor(0x00ffff)
-TPBot.setWheels(100, -100)
-basic.pause(100)
-for (let index = 0; index <= 6; index++) {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        # . . . #
-        . . . . .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        # . . . #
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-}
+TPBot.headlightColor(0xff9da5)
 basic.forever(function () {
-	
+    music.playMelody("C5 A B G A F G E ", 480)
+    TPBot.setWheels(100, 0)
+    basic.pause(5000)
+    music.stopAllSounds()
+    TPBot.stopCar()
+    for (let index = 0; index <= 6; index++) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
 })
